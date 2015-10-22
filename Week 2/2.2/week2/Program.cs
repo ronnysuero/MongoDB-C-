3d51cedce276b9caed96c7ea3282e077f4afdc3c
Ronny Zapata <ronnysuero@gmail.com>
2015-10-22 10:07:32 -0430

    Upload homework week 2

---
+++ Week 2/2.2/week2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync() 
+        {
+            var database = new MongoClient().GetDatabase("students");
+            var collection = database.GetCollection<BsonDocument>("grades");
+
+            List<BsonDocument> listStudents = await collection.Find(new BsonDocument("type", "homework")).ToListAsync();
+            
+            listStudents.GroupBy(data => data["student_id"]).Select(data => new { student_id = data.Key, score = data.Min(x => x["score"]) }).ToList().ForEach(student =>
+            {
+                Console.WriteLine(student + " score removed.");
+                collection.DeleteOneAsync(new BsonDocument("student_id", student.student_id).Add("score", student.score).Add("type", "homework"));
+            });
+        }
+    }
+}
